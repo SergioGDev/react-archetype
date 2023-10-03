06f52eacc984226ea8f93fe4de9e4d6d86b9a159
SergioGDev <admin@sergiogdev.com>
2023-10-03 23:31:53 +0200

    feat: login component and fix template blueprint

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { DashboardRoutes } from "@/modules/Dashboard/router/DashboardRouter"
+import { LoginRoutes } from "@/modules/Login/router/LoginRouter.tsx/LoginRouter"
 import { Route, Routes } from "react-router-dom"
 
 export const AppRouter = () => {
   return (
     <Routes>
         <Route path='/dashboard/*' element={<DashboardRoutes />} />
+        <Route path='/login/*' element={<LoginRoutes />} />
     </Routes>
   )
 }
